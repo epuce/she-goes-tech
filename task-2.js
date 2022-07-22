e5741eb818b9d05c01f03536c99015b21c259c27
epuce <edm.puce@gmail.com>
2022-07-22 19:12:03 +0300

    Update task-2.js
    deleted row

--- task-2.js
+++ task-2.js
@@ -2,7 +2,6 @@ var text="anything"
 var num = 123
 var bool = true
 var arr = [1, true, "", [], undefined, null]
-var obj = {name: "Ed", "is active": true}
 var und
 var empty = null
 var noNumber = "test" * 10
