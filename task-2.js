9b81ff9f6215c7a53f380a6a58b8b26d6d266182
Edmunds Puce <edm.puce@gmail.com>
2022-07-22 18:54:27 +0300

    task-2.js take a look

---
+++ task-2.js
@@ -0,0 +1,8 @@
+var text="anything"
+var num = 123
+var bool = true
+var arr = [1, true, "", [], undefined, null]
+var obj = {name: "Ed", "is active": true}
+var und
+var empty = null
+var noNumber = "test" * 10
