148c2cd07151b54702eb817807c2b0e338d01a6a
Edmunds Puce <edm.puce@gmail.com>
2022-07-22 20:52:38 +0300

    task-3.js done

---
+++ task-3.js
@@ -0,0 +1,25 @@
+var money = 100;
+
+if (money > 0) {
+    alert("You have  " + money + "$")
+} else {
+    alert("No money")
+}
+
+var animal = ""
+
+switch("dog") {
+    case "dog":
+        alert("Dog say woof")
+        break
+    case "cow":
+        alert("Cow says mooo");
+        break
+    default:
+        alert("Nobody know how this animal talks")
+        break
+}
+
+100 > 0 && "text" === "wrong text"
+"text" === "text" || 12 == "12"
+
