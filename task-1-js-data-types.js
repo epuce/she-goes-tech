737d23024285ef52b38d49742f87ab898172d45a
Valda Naujokaitiene <valdanaujokaitiene@Valdas-Air.lan>
2023-02-09 20:21:20 +0200

    Code add

---
+++ task-1-js-data-types.js
@@ -0,0 +1,16 @@
+var name = "Lisa"
+23
+23 > 3
+var listOfLetters = ["a,b,c,d"e]
+var account = {name: "unknown",
+               pass: "word",
+              }
+
+              var user= {name: "John",
+              surname: "Billy",
+             }
+   undefined
+   user.names
+   undefined
+
+   
