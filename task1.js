b0cecf4e32ec1783f7a1dc286ed1de4046e1319f
lindalejiete <116344082+lindalejiete@users.noreply.github.com>
2023-02-09 20:33:58 +0200

    created new file

---
+++ task1.js
@@ -0,0 +1,20 @@
+var name = "LindaLejiete"
+var number = 2
+var boolean = true
+var array = [10,11,12,13,12]
+var object = {name: Linda, age: 99}
+var undefined = 'object'
+var nothing = 'null'
+var nothing = 'null'
+var array = [1, 2, false, [], {}, undefinied]
+var user = {
+    name: 'Linda',
+    email: 'length.l@email.com',
+}
+var admin = {}
+admin.isAdmin = true
+admin.name = 'Jim'
+var missing = undefined
+var notANumber = NaN
+var notThere = null
+var notANumber1 = parseIn("some text")
