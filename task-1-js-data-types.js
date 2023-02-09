0c5d524bc5e132f1a13075a84f17b188578208f3
LindaAlpeLuka <linda.selga@gmail.com>
2023-02-09 20:39:45 +0200

    did the data type task

---
+++ task-1-js-data-types.js
@@ -0,0 +1,17 @@
+var example = "Linda"
+var nextExample = 1
+var otherExample = true
+var anOtherExample = [1, 2, 3, 4]
+var user ={
+    name: "Linda"
+    email: undefined,
+}
+
+var admin = {
+    admin.isAdmin = true
+    admin.name = "Jim"
+}
+
+var missing = undefined
+var noThere = null
+var notANumber = NaN
