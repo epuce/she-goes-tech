deeaf7cb3821a75f7ad02acaca946893c7afbb49
viola <pumpure@yahoo.com>
2023-02-09 12:21:32 -0600

    task

---
+++ script.js
@@ -0,0 +1,11 @@
+var message = ['Hi everybody!'];
+var age = [23];
+var isTrue = true;
+var listToDo = ["gym","breakfast","studies","supermarket","lunch"];
+var cerealBox = { shape: "square", weight: "2kg"}
+var undefinedType
+var zeroVariable = null;
+var textVariable = Number("not a number");
+
+
+
