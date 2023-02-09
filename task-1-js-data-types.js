8d9d789ccef333907ab4137622392d7db8b9c094
Ieva Zvanitaja Rubine <ieva.zvanitaja.rubine@gmail.com>
2023-02-09 20:08:43 +0200

    First task

---
+++ task-1-js-data-types.js
@@ -0,0 +1,14 @@
+var thisIsAString = "Hello";
+var thisIsANumber = 10;
+var thisIsABoolean = true;
+var array = [1, 2, 3, 4, 5];
+var object = {
+    name: "Bob",
+    surname: "Smith"
+}
+var thisIsUndefined;
+var thisIsANull = null;
+var another = NaN;
+
+
+
