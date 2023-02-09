2047dac67f2266a6ee2ffa889f8e1daf014cafed
Agne <agne.buraityte@gmail.com>
2023-02-09 20:15:49 +0200

    addedseveral data types, not 8 but some

---
+++ task-1-js-types.js
@@ -0,0 +1,11 @@
+var myString = "Hello!";
+var myNumber = 123;
+var myBoolean = false; 
+var myArray = [1, 2, 3, 4, 5];
+var myObject = {
+  userName : "Agne",
+  userEmail : "Agne@agne.com"  
+};
+var myUndefined = a;
+var myNull = null;
+var myNaN = NaN; 
