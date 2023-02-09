70c132f81fb3d60c0cbd27982c2af39f26d1e0a7
KristePe <biskyte@hotmail.com>
2023-02-09 22:58:40 +0200

    task-2.js added

---
+++ task-2.js
@@ -0,0 +1,28 @@
+//string
+var myDogName = "Bart";
+
+//number
+var carWheels = 4;
+
+//boolean
+var sun = false;
+
+
+//array
+var trees = [3, 2, "green", 4, 6];
+
+
+//object
+const person = {
+    name: "Kris",
+    age: 99
+}
+
+//undefined
+var years = "12" + 12;
+
+//NaN
+"12" + 12
+
+//null
+var table = null;
