30ea084c06ed3110929a23a461c1d43bd36efdc3
godakond <godakond@gmail.com>
2023-02-09 20:05:41 +0200

    task 1

---
+++ task-1-js-data-types.js
@@ -0,0 +1,8 @@
+const string = "Goda";
+const someNumber = 2;
+const someBoolean = true;
+const someArray = [1,2,3,4,5];
+const someObject = {name: "Goda", age: 10};
+const someUndefined = undefined;
+const someNull = null;
+const someNotaNumber = parseInt("Goda");
