b17a9b8fda48f97671c4effda8c7b1e3d0a261bb
Vaida <linkuviene.vaida@gmail.com>
2023-02-09 20:03:37 +0200

    First task with variables

---
+++ task-1-js-data-types.js
@@ -0,0 +1,12 @@
+let name = 'vaida';
+let age = 34;
+let learning = false;
+let listOfLetters = ['v', 'a', 'i', 'd', 'a'];
+let me = {
+    name: 'vaida',
+    age: 34
+};
+let notExist;
+let nullValue = null;
+let notNumber = NaN;
+
