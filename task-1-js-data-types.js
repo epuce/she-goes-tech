d24a138a377b960720e64b21d5d57f0d6797fa19
ligasteinberga <liga.steinberga@gmail.com>
2023-02-09 20:17:17 +0200

    Create task-1-js-data-types.js

---
+++ task-1-js-data-types.js
@@ -0,0 +1,16 @@
+var name = "Jānis"
+
+var age =  22
+
+var over18 = 22>18
+
+var dienas = ["pirmdiena", "otrdiena", "trešdiena", "ceturtdiena", "piektdiena"]
+
+var contact = {phone: 22320020, name: "Līga"}
+
+var undefined = []
+
+var number = null
+
+var error = 0/0
+
