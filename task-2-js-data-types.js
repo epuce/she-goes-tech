fa489610dfbfa14e1b96c26b9f418b898c8e89d0
Asta Matačiūnaitė <a840258@yara.com>
2023-02-10 14:34:15 +0200

    finish-task-1

---
+++ task-2-js-data-types.js
@@ -0,0 +1,32 @@
+var name = "Ed"
+var name = 'John'
+
+var num = 234
+
+var bool = true
+var bool = false
+
+var arr = [1, 2, 3, [], false]
+var arr1 = [] ;
+arr1.push(1)
+arr1.push(2)
+arr1.push(3)
+arr1.push(4)
+arr1.push(5)
+
+var user = {
+    name: 'Ed', 
+    email: undefined,
+}
+
+var admin = {}
+    admin.isAdmin = true
+    admin.name = 'Jim'
+
+    var missing;
+    var missing1 = undefined;
+
+    var notThere = null;
+
+    var notANumber = NaN;
+    var notANumber1 = parseInt('some text');
