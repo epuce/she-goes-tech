ce9aef99922a6bc0e4897d3cedbb681a23ffa5fa
LindaAlpeLuka <linda.selga@gmail.com>
2023-02-12 22:00:42 +0200

    updated initial checkup

--- task-1-js-data-types.js
+++ task-1-js-data-types.js
@@ -1,17 +1,16 @@
-var example = "Linda"
-var nextExample = 1
-var otherExample = true
-var anOtherExample = [1, 2, 3, 4]
+var exampleString = "Linda";
+var exampleNumber = 1;
+var exampleBoolean = true;
+var exampleArray = [1, 2, 3, 4];
 var user ={
-    name: "Linda"
-    email: undefined,
-}
+    name: "Linda",
+    email: "test@test.com",
+};
 
-var admin = {
-    admin.isAdmin = true
-    admin.name = "Jim"
-}
+var admin = {};
+    admin.isAdmin = true;
+    admin.name = "Jim";
 
-var missing = undefined
-var noThere = null
-var notANumber = NaN
+var missing = undefined;
+var noThere = null;
+var notANumber = parseInt("Linda");
