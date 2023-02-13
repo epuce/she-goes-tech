9c67d0a119a64919a1cb1865d0262fcea6cd1a4b
Karolina Masharo <karolinamasharo@gmail.com>
2023-02-13 14:16:09 +0200

    add task-1-js-data-types.js

---
+++ task-1-js-data-types.js
@@ -0,0 +1,12 @@
+var name = "Karolina"
+var age = 27
+var boolean = false
+var cities = ["Vilnius", "Kaunas", "Klaipeda", "Kernave", "Siauliai"]
+
+var user = {
+    name: "Karolina",
+    email: kk@kk.com
+}
+var missing;
+var notThere = null;
+var notANumber = NaN;
