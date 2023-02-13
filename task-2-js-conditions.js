2b3ec3340157a57ea3e1fcd15e0d1d073d40c221
oktimeforplanb <bliucijab@gmail.com>
2023-02-13 20:28:16 +0200

    Changed task-2-js-conditions.js

---
+++ task-2-js-conditions.js
@@ -0,0 +1,25 @@
+var money = 5;
+if (money>0){
+    alert ('You have *money*$')
+} else {
+    alert ('You have no money')
+}
+
+var animal = "cat"
+switch(animal){
+    case "dog":
+        alert("Dog says wooof")
+        break
+    case "cat":
+        alert("Cat says meww")
+        break
+    case "cow":
+        alert("Cow says moo")
+        break
+    default:
+        alert("this animal does not speak")
+    }
+    5>2 && 3>1
+
+    5>2 || 3 <5
+    
