6de031a7cee622f9c5f521be93d7dff670d4561d
Justina Greblikaite <justinagreblikaite@Justinas-MacBook-Air.local>
2023-02-13 20:54:12 +0200

    first task

---
+++ task-1-js-data-types.js
@@ -0,0 +1,8 @@
+var Name = "Justina"
+var Number = 7
+var boolean = true
+var listOfStuff = [1, 2, 3, "four", 5]
+var Object = {phone: 333777, email: "she@she.com"}
+var notToUnderstand = undefined
+var zero = null
+var withoutNum = NaN
