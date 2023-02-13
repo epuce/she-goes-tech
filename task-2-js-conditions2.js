5b7148d47c5accae9f80a8ffeaa6426173d8ccc8
AivijaN <124625096+AivijaN@users.noreply.github.com>
2023-02-13 22:10:48 +0200

    Update task-2-js-conditions2.js

--- task-2-js-conditions2.js
+++ task-2-js-conditions2.js
@@ -1,1 +1,11 @@
+var money = 0;
+if (money > 0) { alert ('You have money"$"')} else {
+    alert('You have no money')}
 
+var animal = 'bird';
+switch(animal) {case 'dog': alert ('Dog says something) break case 'cat':
+        alert('Cat says something') break Default: alert('Im not animal!')}
+
+10>1 && 10>2
+true || false
+if (10>1 && 10>2) {alert ('10 is the biggest')} else if (true || false) {alert('If one is true, all is true')}
