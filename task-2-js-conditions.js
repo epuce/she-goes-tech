4ce0d692bf5dc1b3ba186dce3291609ae221a824
Alina Lucane <akalasnikova26@gmail.com>
2023-02-13 20:49:24 +0200

    task2

--- task-2-js-conditions.js
+++ task-2-js-conditions.js
@@ -1,23 +1,25 @@
 var money;
 if(money > 0) {
-    alert('You have *money*$')
+    alert('You have ' + money +'$')
 } else {
     alert('You have no money')
 }
+
+let animal ="cat";
 switch(animal){
     case "cat":
-        alert("cat makes mooau mooua")
+        alert("cat says mooau mooua")
             break;
     case "dog":
-        alert("dog makes woof woof")
+        alert("dog says woof woof")
             break;
     default:
-        alert("this is not an animal")
+        alert("this does not speak")
 }
 
 if( 10>4 && 8>4){
     alert("10 is bigger")
 }
 if(1 !==2 || 0!==2){
-    alert("It is true")
+    alert("It is not 2")
 }
