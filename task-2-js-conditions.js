0c312bbe70b97267806e14ec659d61f55d46745e
AivijaN <124625096+AivijaN@users.noreply.github.com>
2023-02-13 20:42:19 +0200

    Create task-2-js-conditions.js

---
+++ task-2-js-conditions.js
@@ -0,0 +1,13 @@
+var money = 10;
+undefined
+if (money>0) { alert ('you have "money"$')} else { alert('You have no money')}
+undefined
+
+var animal = 'cat';
+switch(animal) {case 'dog': alert ('Dog say something') break case 'cat': alert ('Cat says something') break default: alert ('Not animal')}
+}
+
+var age = 10;
+var name = 'Jen';
+if (age=10 && name='Jen'{alert (Hello world)} else if (age<10 || name !=='Jen') {alert (Really sorry)}
+
