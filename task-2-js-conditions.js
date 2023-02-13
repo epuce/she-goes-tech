5e031fc933440a6d3b336df37aaa8754e49c3524
Justina Greblikaite <justinagreblikaite@Justinas-MacBook-Air.local>
2023-02-13 20:32:44 +0200

    new file 2

---
+++ task-2-js-conditions.js
@@ -0,0 +1,36 @@
+var money = 5
+if (money > 0) {
+    alert ("you have *money*$")
+} else {
+    alert ("You have no money")
+}
+
+var animal = "cat"
+switch (cat) {
+    case "cat":
+        alert ("cat say miaw")
+        break
+    case "dog":
+        alert ("dog say woof")
+        break
+    case "duck":
+        alerrt ("duck says kva")
+        break
+    default:
+        alert ("no animal inserted")
+        break        
+}
+
+if ((10>5) && (5>2)) {
+    alert ("true")
+}
+else {
+    alert ("false")
+}
+
+if ((10>5) || (5>2)) {
+    alert ("true")
+}
+else {
+    alert ("false")
+}
