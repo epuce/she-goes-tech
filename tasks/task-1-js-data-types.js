87a4eafca3bad3f99cb9788d10ba1c9ce25c9b13
KristianaTR <kristiana.tatarcuka@gmail.com>
2023-02-13 16:45:06 +0200

    task-1

---
+++ tasks/task-1-js-data-types.js
@@ -0,0 +1,11 @@
+var name = "Kristiana";
+var age = 40;
+var happy = true;
+var hobbiesList = ['fitness', 'landscaping', 'sewing', 'sketchup', 'handcrafting'];
+var kidsNames = {
+    daughter: 'Līva',
+    son: 'Toms'
+};
+var achievements;
+var lostEnthusiasm = null;
+var notNumber = 'tskcjsdjfhsdkf';
