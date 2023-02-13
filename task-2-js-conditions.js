e30624db7c66af28773e9638906202d8d46bde45
Ieva Zvanitaja Rubine <ieva.zvanitaja.rubine@gmail.com>
2023-02-13 20:26:43 +0200

    Task 2

---
+++ task-2-js-conditions.js
@@ -0,0 +1,32 @@
+var money = 500;
+
+if (money > 0) {
+    alert ("You have money")
+} else {
+    alert ("You have no money")
+}
+
+var animal = "cat";
+
+switch(animal) {
+    case "dog":
+        alert ("Dog says woof!")
+        break;
+    case "cat":
+        alert ("Cat says meow!")
+        break;
+    default:
+        alert ("You are not an animal")
+        break;
+}
+
+var age = 18;
+var userName = "John";
+
+if (age >= 18 && userName === "John") {
+    alert ("Welcome")
+} else if (age <= 18 || name !== "John") {
+    alert ("Are you realy welcome?")
+} else {
+    alert ("Go away!")
+}
