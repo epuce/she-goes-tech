d2f2911f33b2566bf3ca09d2e4bd6e1535e853a9
LindaAlpeLuka <linda.selga@gmail.com>
2023-02-13 20:29:48 +0200

    did the first attempt for the task 2 conditions

---
+++ task-2-js-conditions.js
@@ -0,0 +1,22 @@
+var money = 10
+if (money > 0) {
+    alert ("You have money")
+}   else {
+    alert ("You have no money")
+}
+
+var animal = "cat"
+switch(animal) {
+    case "cat":
+        alert("mjau")
+        break
+    case "dog":
+        alert("woof")
+        break
+    case "cow":
+        alert("moo-moo")
+        break
+}
+
+20 > 6 && 10 > 5
+20 < 6 || 10 > 5
