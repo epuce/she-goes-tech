4419d0e1c8211cb33623a8ce87229ebaf7a09c16
viola <pumpure@yahoo.com>
2023-02-13 12:39:56 -0600

    conditions checkup

---
+++ task-2-js-conditions.js
@@ -0,0 +1,26 @@
+var money = 5
+if (money>0) {alert ('You have *money*$')} 
+else {alert ('You have no money')};
+
+
+switch (animal) {
+    case 'dog':
+        alert ('dog says woof')
+        break;
+    case 'cat':
+        alert ('cat says meow')
+        break;
+    case 'fish':
+        alert ('fish says nothing')
+        break;
+    default:
+        alert ("animals don't speak")
+        break;
+
+}
+
+var money = 10
+var age = 100
+if (money > 1000000 && age > 100) {alert ('you are rich and old')}
+
+if (money > 100000 || age > 100) {alert ('you are rich or old')}
