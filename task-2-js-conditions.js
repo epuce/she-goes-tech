78dac062c289647d8204a057096d173b80dbf71c
Valda Naujokaitiene <valdanaujokaitiene@Valdas-Air.lan>
2023-02-13 20:38:29 +0200

    Conditions

---
+++ task-2-js-conditions.js
@@ -0,0 +1,26 @@
+var Money = 5
+undefined
+if (Money > 0) {
+    alert ("*money*$")
+} else {
+    alert ("You have no money")
+}
+
+var animal = "Cow"
+undefined
+switch (animal){
+    case ("Snake"):
+        alert ("Ssssss")
+        break
+    case ("Cow"):
+        alert ("Mooo")
+        break
+    default:
+        alert ("I don't know")
+}
+
+5>3 && 3>5
+
+4<3 || 3>2
+true
+
