4b7139b44b0b3027621eda7866acf6148b8a1615
KristianaTR <kristiana.tatarcuka@gmail.com>
2023-02-13 20:29:34 +0200

    task-2

---
+++ tasks/task-2-js-conditions.js
@@ -0,0 +1,31 @@
+var money;
+if (money > 0 ) {
+    console.log('You have' *money*$);
+}
+else {
+    console.log('You have no money');
+}
+
+var animal = mouse;
+switch (animal) {
+    case 'cat' : 
+        return ('The cat says meou')
+    break;
+    case 'dog':
+        return ('The dog says woow')
+    break;
+    case 'mouse' :
+        return ('The mouse says sqeek')
+    break;
+    default:
+        return ('Your animal is very silent')
+    break;
+}
+
+var number;
+if (number > 0 && number < 10) {
+    console.log('You are lucky');
+}
+if (number < 0 || number === 0) {
+    console.log('You are not lucky');
+}
