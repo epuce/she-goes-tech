2820204c457f90a5b878c0765cb94b2480135b0e
Vaida <linkuviene.vaida@gmail.com>
2023-02-13 20:26:15 +0200

    Updated task 2

---
+++ task-2-js-conditions.js
@@ -0,0 +1,35 @@
+let money = 5;
+
+if (money > 0){
+    alert ("You have " + money + "$") 
+} else {
+    alert ('You have no money')
+}
+
+
+var animal = 'dog';
+var animalNoise = 'woof'
+
+switch(animal){
+    case 'cat':
+        alert ('cat says meow')
+            break;
+    case 'dog':
+        alert ('dog say woof')
+            break;
+    default:
+        alert ("This animal does not speak")
+}
+
+if(5>1 && 1>0){
+    alert ('True')
+} else{
+    alert ('False')
+}
+
+if(10>5 || 1<4){
+    alert ("Hello world")
+}else {
+    alert ("Not hello at all")
+}
+
