bdb761be66dcac02bfe488e6763bc9982d643031
ZymanteL <zymantelelesiute@gmail.com>
2023-02-13 13:21:55 +0200

    New file added task-1-js-data-types.js

--- task-1-js-data-types.js
+++ task-1-js-data-types.js
@@ -1,32 +1,18 @@
-var name = "John"
-var name1 = 'John'
+var name = "Žymantė";
 
-var num = 412
+var num = 27;
 
-var bool = true
-var bool1 = false
+var studyingStatus = true;
 
-var arr = [1, false, [], {}, undefined]
-var arr1 = [];
-arr1.push(1)
-arr1.push(2)
-arr1.push(3)
-arr1.push(4)
-arr1.push(5)
+var yearOfStudy = ["first", "second", "third", "fourth", "fifth"];
 
-var user = {
-    name: 'Ed',
-    email: undefined,
+var studylenguages = {
+    frontEnd: "JavaScript",
+    backEnd: "PHP",
 }
 
-var admin = {}
-admin.isAdmin = true
-admin.name = 'Jim'
+var graduationYear = undefined;
 
-var missing;
-var missing1 = undefined;
+var tuitionFee = null;
 
-var notThere = null;
-
-var notANumber = NaN;
-var notANumber1 = parseInt("some text");
+var hobbies = NaN;
