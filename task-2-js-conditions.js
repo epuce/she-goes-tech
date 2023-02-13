aa95def8fd7d336d9a2ba27de3b875d7130e5798
Aiste Gilyte <aigilyt@gmail.com>
2023-02-13 20:30:09 +0200

    edit some files

---
+++ task-2-js-conditions.js
@@ -0,0 +1,29 @@
+if var money > 0 {
+    alert('You have *money*$')
+} else {
+    alert('You have no money')
+}
+switch (animal) {
+    case("dog"):
+    alert('dog says whoof')
+    break
+    case("cat")
+    alert('cat says meow')
+    break
+    case("cow")
+    alert('cow says moo')
+    break
+    default:
+        alert('this animal does not speak')
+        break
+}
+if (10>5&&5==5) {
+    alert('response is valid')
+} else {
+    alert('I don't know')
+}
+if (11>6||3===3) {
+alert('response is valid')
+} else {
+    alert('I don't know)
+}
