230cfeeaf2d011270b3f2cc9f8edac8956643dbb
neringa1991 <neringa1991@yahoo.com>
2023-02-13 20:29:36 +0200

    add template literals

--- task-3.js
+++ task-3.js
@@ -2,17 +2,17 @@ const money = 10;
 const animal = "cat";
 
 if (money > 0) {
-  console.log("You have money");
+  console.log(`You have ${money}$`);
 } else {
   console.log("You have no money");
 }
 
 switch (animal) {
   case "cat":
-    console.log("Animal says meowww");
+    console.log(`${animal} says meowww`);
     break;
   case "dog":
-    console.log("Animal says auuu");
+    console.log(`${animal} says auuu`);
     break;
   default:
     console.log("There's no such sound");
