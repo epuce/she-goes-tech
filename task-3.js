b0303c0a5f66ca6f47c0cda03e36e0b8043e37ed
ligasteinberga <liga.steinberga@gmail.com>
2023-02-14 08:01:02 +0200

    Create task-3.js

---
+++ task-3.js
@@ -0,0 +1,28 @@
+var money = 10
+
+if (money>0) {
+    alert("You have *money*$")
+} else {"You have no money"}
+
+
+var animal = "dog";
+switch(animal) {
+case "dog":
+    alert("dog says woof")
+    break;
+    case "cat":
+        alert("cat says meow")
+        break;
+        case "frog":
+            alert("frog says ribbit")
+            break;
+            default:
+                alert("this animal can't speak")
+}
+
+10>1 && 4<20
+
+var x = 6;
+var y = 1;
+(x == 6 || y == 0)
+
