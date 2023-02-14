74ad5e7bb60101f8820781d715433ccb0b20c8e5
Asta Matačiūnaitė <a840258@yara.com>
2023-02-14 16:59:00 +0200

    task-2-js-conditions.js

---
+++ task-2-js-conditions.js
@@ -0,0 +1,35 @@
+var money = 0;
+
+if (money > 0) {
+   alert("You have" + money + "&")
+} else {
+    alert("You dont have money")
+}  
+
+var animal = "chicken"
+
+switch (animal) {
+  case "dog":
+       alert("Dog says huh")
+       break
+  case "cow":
+       alert("Cow says muuh") 
+       break
+  case "donkey":
+       alert("Donkey says y-a")
+       break
+  default:
+       alert("It is not an animal")          
+}
+
+if(5 >3 && 10 < 12) {
+   alert ("true")
+} else {
+   alert ("false")
+}
+
+if(10 > 1 || 6 > 3) {
+  alert("true")
+ } else {
+   alert ("false")
+}
