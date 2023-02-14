5a428a00f65092b7465ef1fd9d352b19f113595a
neringa1991 <neringa1991@yahoo.com>
2023-02-14 21:21:43 +0200

    add file task-3-html-tags.html

---
+++ task-1.js
@@ -0,0 +1,20 @@
+"use strict";
+var myName = "Neringa";
+var myAge = 31;
+var isOld = false;
+var animals = ["cat", "dog", "bird", "rabbit", "lion"];
+var relativesNames = {mother: "Daina", father: "Gytis"};
+var cat;
+var nullValue = null;
+var convertedNumber = parseInt("catgjkhkfl");
+
+console.log(
+  typeof myName,
+  typeof myAge,
+  typeof isOld,
+  typeof animals,
+  typeof relativesNames,
+  typeof cat,
+  typeof nullValue,
+  typeof convertedNumber
+);
--- task-2.js
+++ task-2.js
@@ -1,20 +1,27 @@
-"use strict";
-var myName = "Neringa";
-var myAge = 31;
-var isOld = false;
-var animals = ["cat", "dog", "bird", "rabbit", "lion"];
-var relativesNames = {mother: "Daina", father: "Gytis"};
-var cat;
-var nullValue = null;
-var convertedNumber = parseInt("catgjkhkfl");
+const money = 10;
+const animal = "cat";
 
-console.log(
-  typeof myName,
-  typeof myAge,
-  typeof isOld,
-  typeof animals,
-  typeof relativesNames,
-  typeof cat,
-  typeof nullValue,
-  typeof convertedNumber
-);
+if (money > 0) {
+  console.log(`You have ${money}$`);
+} else {
+  console.log("You have no money");
+}
+
+switch (animal) {
+  case "cat":
+    console.log(`${animal} says meowww`);
+    break;
+  case "dog":
+    console.log(`${animal} says auuu`);
+    break;
+  default:
+    console.log("There's no such sound");
+    break;
+}
+
+5 > 1 && 6 > 3
+  ? console.log("That's correct")
+  : console.log("That's not correct");
+0 > 1 || 2 > 3
+  ? console.log("That's correct")
+  : console.log("That's not correct");
