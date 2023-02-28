9a3e50cc979a6a9d008e61be403c133655cc45a5
oktimeforplanb <bliucijab@gmail.com>
2023-02-28 19:43:57 +0200

    task-6-javascriot-loops.js

---
+++ task-6-javascriot-loops.js
@@ -0,0 +1,22 @@
+for (var i=10; i<=20; i=i+2) {
+    console.log(i)
+}
+
+var list = ['a','b','c','d','e'];
+
+list.forEach(function(char) {
+    console.log(char)
+})
+
+['a','b','c','d', 'e'].forEach(function(char) [
+console.log(char)
+])
+
+[11,12,13,14,15].map(function(num) {
+    return num**2
+})
+
+var array = [];
+
+while(array.length<5) {array.push(Math.random())}
+
