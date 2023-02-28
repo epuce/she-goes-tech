c94c23ddf28d796b7e86503b0dfb6f47ded63cb0
Karolina Masharo <karolinamasharo@gmail.com>
2023-02-28 19:37:18 +0200

    Add task 6

---
+++ task-6-javascript-loops.js
@@ -0,0 +1,19 @@
+for (let i=10; i<=20; i+=2){
+    console.log(i)
+};
+
+[1,2,3,4,5].forEach(function(num) {
+    console.log(num)
+});
+
+var array = [1, 2, 3, 4];
+
+var multArray = array.map(function(value) {
+    return value*value
+    console.log(multArray)
+});
+
+var list = [];
+for(i=0; i<5; i++) {
+    list.push(i)
+};
