1c5f32fb94625179702c8574690282fbf57a362e
ZymanteL <zymantelelesiute@gmail.com>
2023-02-28 19:09:23 +0200

    Changed task-8.js

--- task-8.js
+++ task-8.js
@@ -2,30 +2,33 @@
 
 let number = 10;
 while (number < 20) {
-    if (number%2 === 0) {
-        number++;
-    } else {
-        console.log (number);
-        number++;
-    } 
+  if (number % 2 === 0) {
+    number++;
+  } else {
+    console.log(number);
+    number++;
+  }
 }
 
 // loop thru an array of 5 values and print out it's content
 
-let fiveValues = [20, 88, 56, 44, 45]
+let fiveValues = [20, 88, 56, 44, 45];
 
-fiveValues.forEach(value => {
-    console.log (value);
+fiveValues.forEach((value) => {
+  console.log(value);
 });
 
 // modify array of 5 numbers by multiplying the numbers by itself
 
-fiveValues.forEach(value => {
-    console.log (value*value);
+fiveValues.map((value) => {
+  console.log(value * value);
 });
 
 // Create a loop that will add 5 values to an empty array
 
 let newData = [];
 
-newData.push("Tomas", "Agne", "Vilius", "Rima");
+for (var i = 0; i < 5; i++) {
+    newData.push(i);
+}
+
