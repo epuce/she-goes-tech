cd748f329b0b8768169215c637b7b506292b8c59
Vaida <linkuviene.vaida@gmail.com>
2023-02-28 19:12:29 +0200

    Check up of javascript loops

---
+++ task-6-javascript-loops.js
@@ -0,0 +1,26 @@
+// Task 1
+for(let i=10; i<=20; i=i+2){
+    console.log(i)
+}
+
+// Task2
+let arr = ['a', 'b', 'c', 'd', 'e'];
+arr.forEach(function(value){
+    console.log(value)
+})
+
+// Task3
+let num = [12, 14, 55, 6585, 54];
+num.map(function(value){
+    return value*value;
+})
+
+// Task4
+let empty = [];
+for( let i = 10; i<15; i++){
+    empty.push(i);
+}
+console.log(empty);
+
+
+
