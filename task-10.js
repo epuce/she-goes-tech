f1b88e6364de828178874516b237f706bd922042
Valda Naujokaitiene <valdanaujokaitiene@Valdas-Air.lan>
2023-03-02 21:25:13 +0200

    Task 10

---
+++ task-10.js
@@ -0,0 +1,15 @@
+" "
+
+
+localStorage.name = "Valda"
+
+
+var Object = "name"
+window.localStorage.setItem("Object", JSON.stringify(Object));
+
+localStorage
+Storage {Object: '"name"', length: 1}
+
+
+
+
