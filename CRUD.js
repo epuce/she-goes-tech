b816bf72eb39ada6d5544c55882d0499910bd265
Edmunds Puce <i-am-funny@yahoo.com>
2023-03-06 21:26:42 +0200

    CRUD stage 2

--- CRUD.js
+++ CRUD.js
@@ -1,22 +1,42 @@
 function saveForm() {
-    localStorage.userList = JSON.stringify(list)
-    var name = document.querySelector('.js-name').value
-    var email = document.querySelector('.js-email').value
-    
-    var user = {
-        name: name,
-        email: email,
+    var nameInput = document.querySelector('.js-name')
+    var emailInput = document.querySelector('.js-email')
+    var isFormValid = true;
+
+    if (nameInput.value.length < 3) {
+        nameInput.classList.add('validation-error')
+        isFormValid = false
+    } else {
+        nameInput.classList.remove('validation-error')
     }
 
+    if (isFormValid) {
+        var user = {
+            name: nameInput.value,
+            email: emailInput.value,
+        }
+    
+        var list = getUserList()
+    
+        list.push(user)
+    
+        localStorage.userList = JSON.stringify(list)
+    
+        nameInput.value = ''
+        emailInput.value = ''
+    
+        renderTable()
+    }
+}
+
+function getUserList() {
     try {
         var list = JSON.parse(localStorage.userList)
     } catch {
         var list = []
     }
 
-    list.push(user)
-
-    localStorage.userList = JSON.stringify(list)
+    return list;
 }
 
 document.querySelector('.js-save')
@@ -31,3 +51,27 @@ document.querySelectorAll('input').forEach(function(input) {
         }
     })
 })
+
+function renderTable() {
+    var list = getUserList()
+    var tableContent = ''
+
+    list.forEach(function(user, index) {
+        var row = `
+            <tr>
+                <td>`+(index+1)+`</td>
+                <td>`+user.name+`</td>
+                <td>`+user.email+`</td>
+            </tr>
+        `
+        // `<td>`+index+`</td>` = "<td>0</td>"
+        // `<td>`+index+`</td>` = "<td>1</td>"
+        // "text" + 123 + "text" = "text123text"
+
+        tableContent = tableContent + row
+    })
+
+    document.querySelector('.js-user-table').innerHTML = tableContent
+}
+
+renderTable()
