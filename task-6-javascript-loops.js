55e273575f58e47d7010c2f82e7fac20535dcf71
Asta Matačiūnaitė <a840258@yara.com>
2023-03-06 10:06:54 +0200

    task-6-javascript-loops.js

---
+++ task-6-javascript-loops.js
@@ -0,0 +1,20 @@
+for (var i=10; i<=20; i=i+2) {
+  console.log(i)
+}
+
+var list = ["Liz" , "Ruth", "Berta"];
+
+list.forEach(function(name) {
+  console.log(name)
+})
+
+[2,3,4,5,6].map(function(num) {
+  return num*num 
+})
+
+var array = [];
+
+while(array.length<5){
+  array.push(Math.random())
+}
+
