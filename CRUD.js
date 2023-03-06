43623ac2790b63998476ca753c9cbb0f20942986
lindalejiete <116344082+lindalejiete@users.noreply.github.com>
2023-03-06 21:51:04 +0200

    CRUD task 060323

---
+++ CRUD.js
@@ -0,0 +1,77 @@
+function saveForm() {
+    var nameInput = document.querySelector('.js-name')
+    var emailInput = document.querySelector('.js-email')
+    var isFormValid = true;
+
+    if (nameInput.value.length < 3) {
+        nameInput.classList.add('validation-error')
+        isFormValid = false
+    } else {
+        nameInput.classList.remove('validation-error')
+    }
+
+    if (isFormValid) {
+       var user = {
+            name: nameInput.value,
+            email: emailInput.value,
+        }
+
+        var list = getUserList()
+
+        list.push(user)
+
+        localStorage.userList = JSON.stringify(list)
+
+        nameInput.value = ''
+        emailInput.value = ''
+
+        renderTable()
+    }
+}
+
+function getUserList() {
+    try {
+        var list = JSON.parse(localStorage.userList)
+    } catch {
+       var list = []
+    }
+
+    return list;
+}
+
+document.querySelector('.js-save')
+    .addEventListener('click', function() {
+       saveForm()
+    })
+
+document.querySelectorAll('input').forEach(function(input) {
+    input.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13) {
+            saveForm()
+           }
+        })
+    })
+
+function renderTable() {
+    var list = getUserList()
+    var tableContent = ''
+
+    list.forEach(function(user, index) {
+        var row = `
+            <tr>
+                <td>`+(index+1)+`</td>
+                <td>`+user.name+`</td>
+                <td>`+user.email+`</td>
+            </tr>
+            `
+
+
+
+
+    tableContent = tableContent + row
+    })
+
+document.querySelector('.js-user-table').innerHTML = tableContent
+}
+
+renderTable()
