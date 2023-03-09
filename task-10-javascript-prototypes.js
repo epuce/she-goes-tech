827b79eebb2bb62c0b6df8fe6f7495d5092d64e7
ZymanteL <zymantelelesiute@gmail.com>
2023-03-09 21:34:06 +0200

    Changed task-10-javascript-prototypes.js

--- task-10-javascript-prototypes.js
+++ task-10-javascript-prototypes.js
@@ -1,3 +1,6 @@
+console.log(Math.max(3,4,5,6,88))
+console.log(Date());
+
 function Book(name, author, pages) {
   this.name = name;
   this.author = author;
@@ -8,18 +11,23 @@ function Book(name, author, pages) {
   };
 
   this.whoWritenthis = function () {
-    console.log("This book is writen by" + this.author);
+    console.log("This book is written by " + this.author);
   };
 }
 
 var book1 = new Book("Murder on the Orient Express", "Agatha Christie", 256);
-book1.whoWritenthis;
+book1.whoWritenthis();
+book1.howManypages();
 
 function SummerRead() {
-  Book.apply(this.arguments);
+  Book.apply(this, arguments);
 
-  this.howManyDaysToRead = () => console.log(this.howManypages / 20);
+  this.SummerRead = true;
+
+  this.howManypages = function () {
+    return console.log("This you'll read for " + this.pages / 20 + " days");
+  }
 }
 
 var book2 = new SummerRead("Philosopher's Stone", "J. K. Rowling", 223);
-book2.howManyDaysToRead;
+book2.howManypages();
