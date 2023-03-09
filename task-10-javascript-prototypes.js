65203578a2235bd4e03e8f6513e4d2a9749f8ba7
ZymanteL <zymantelelesiute@gmail.com>
2023-03-09 21:03:23 +0200

    Adedd task-10-javascript-prototypes.js

---
+++ task-10-javascript-prototypes.js
@@ -0,0 +1,26 @@
+function  Book(name, author, pages) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+
+    this.howManypages = function () {
+        console.log("This book has" + this.pages);
+    }
+
+    this.whoWritenthis = function () {
+        console.log("This book is writen by" + this.author);
+    }
+
+}
+
+var book1 = new Book("Murder on the Orient Express", "Agatha Christie", 256 )
+book1.whoWritenthis
+
+function SummerRead() {
+    Book.apply(this. arguments)
+
+    this.howManyDaysToRead= () => console.log(this.howManypages/20); 
+
+}
+
+var book2 = new SummerRead("Philosopher's Stone", "J. K. Rowling", 223)
