ff7e3d42b2ec006ab1f0bd9e31432d7a2ceebf83
Ieva Zvanitaja Rubine <ieva.zvanitaja.rubine@gmail.com>
2023-03-09 21:36:43 +0200

    prototypes

--- CRUD with prototypes.js
+++ CRUD with prototypes.js
@@ -7,7 +7,7 @@ var nameInput =
     /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     this.setName = function(newName) {
-      this.name - typeof newName === 'string' ? newName : '';
+      this.name = typeof newName === 'string' ? newName : '';
     }
 
     this.setEmail = function (newEmail) {
--- task-10-javascript-prototypes.js
+++ task-10-javascript-prototypes.js
@@ -4,6 +4,8 @@ var randomNumber = Math.random() * 100;
 var roundedNumber = Math.round(randomNumber);
 console.log(roundedNumber);
 
+Object.assign(user, userJim) /* to merge users. first one is the user that is merged into. outcome for user - ame: 'Jim', email: 'test@test.com', age: 111, for userJim - stays the same*/
+
 /*Create your own prototype*/
 
 function Employee (name, age, salary, workdays) {
@@ -31,6 +33,9 @@ function Employee (name, age, salary, workdays) {
     Employee.apply(this, arguments) 
     this.occupation = "salesman"
 
+    this.sayHello = function () {
+        return "Welcome" + this.name
+    }
     this.announceOccupation = function () {
         return "You are a " + this.occupation
     }
@@ -42,3 +47,54 @@ var salesmanBob = new Salesman ("Bob", 50, 2000, ["Tuesday", "Sunday"])
 
 salesmanBob.sayHello()
 salesmanBob.announceOccupation()
+
+
+/* other examples */
+var user = {
+    name: "Ed",
+    email: "test@test.com"
+}
+
+var userJim = {
+    name: "Jim",
+    age: 111
+}
+
+Math.floor(1.7)
+
+function User(name, age) {
+    this.name = name
+    this.age = age
+
+    this.sayHi = function() {
+        return "Hello, my name is" + this.name
+    }
+
+    this.iWasBornIn = function() {
+        var thisYear = new Date().getFullYear()
+
+        if (thisYear - this.age > 2000) {
+            return "I was born after year 2000"
+        } else {
+            return "I was born before year 2000"
+        }
+    }
+}
+
+var user1 = new User("Ed", 65);
+user.sayHi();
+user.iWasBornIn()
+
+function Admin() {
+    User.apply(this, arguments)
+
+    this.isAdmin = true;
+
+    this.sayHi = function() {
+        return "I don't like people"
+    }
+}
+
+var admin1 = new User("Ed", 65);
+admin1.sayHi();
+admin1.iWasBornIn()
