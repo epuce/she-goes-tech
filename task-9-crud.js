70f5b458efa729f8c3f0f1248490721cb8aa4249
Agne <agne.buraityte@gmail.com>
2023-03-09 18:00:01 +0200

    add task on js crud

--- task-9-crud.js
+++ task-9-crud.js
@@ -1,7 +1,7 @@
 var task = document.querySelector(".js-task");
 
 function saveForm() {
-  // TODO: simplify to use.value less   // var XYZ = {   //   task: task.value };
+  // TODO: simplify to use.value less   // var XYZ = {   //   task: task.value }; or should I have taskRow var here
   var list = getTaskList();
 
   if (saveBtn.dataset.index) {
@@ -36,56 +36,55 @@ saveBtn.addEventListener("click", function () {
 
 function renderList() {
   var list = getTaskList();
-  var listContent = ""; //  this may be wrong check, if these 2 var should be inside the function or out. Error message if kept here,
+  var listContent = ""; 
 
-  list.forEach(function (taskRow) {
-    // confused on var names here - should I have an equivalent to user, a new var or sth else?
+  list.forEach(function (taskRow, index) {
     var row =
       `
 <li> ` +
-      taskRow.task +
-      ` </li> 
+      taskRow +
       `
-    //  ` <button class="js-modify"
-    // // data-index="` +
-    //       // index +
-    //       `">
-    // Modify
-    // </button>
-    // <button class="js-done"
-    //  data-index="` +
-    //       index +
-    //       `">
-    //  Done
-    // </button> </li>` when adding this code, remove </li>' above
+ <button class="js-modify"
+    data-index="` +
+      index +
+      `">
+    Modify
+    </button>
+    <button class="js-done"
+     data-index="` +
+      index +
+      `">
+     Done
+    </button> </li>`;
 
     listContent = listContent + row;
-  });
+    
+    document.querySelector(".js-list").innerHTML = listContent;
+
+// Deletion works
+  document.querySelectorAll('.js-list .js-done').forEach(function(button) {
+      button.addEventListener('click', function() {
+          var taskList = getTaskList();
+          taskList.splice(button.dataset.index, 1)
+          console.log(button.dataset.index)
+          localStorage.taskList = JSON.stringify(taskList);
+          renderList()
+        })
+  })
+
+  // TODO: review - sth not adding up. Editing not working
+  //   document.querySelectorAll('.js-list .js-modify').forEach(function(button) {
+  //     button.addEventListener('click', function() {
+  //       var list = getTaskList()
+  //               var taskRow = list[button.dataset.index]
+
+  //               task.value = listContent.task
+
+  //         saveBtn.dataset.index = button.dataset.index
+  //     })
+  })
+
 }
+renderList()
 
-document.querySelector(".js-list").innerHTML = listContent;
 
-// TODO: update js for delete and edit with new names
-// document.querySelectorAll(".js-list .js-done")
-// .forEach(function (button) {
-//   button.addEventListener("click", function () {
-//     var list = getTaskList();
-
-//     list.splice(button.dataset.index, 1);
-//     localStorage.taskList = JSON.stringify(list);
-//     renderList();
-//   });
-// });
-
-// document.querySelectorAll(".js-list .js-modify").forEach(function (button) {
-//   button.addEventListener("click", function () {
-//     var list = getTaskList();
-//     var ??? = list[button.dataset.index];
-
-//     taskInput.value = ???.task;
-
-//     saveBtn.dataset.index = button.dataset.index;
-//   });
-// });
-
-renderList();
