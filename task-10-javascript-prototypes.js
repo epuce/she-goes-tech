50402b3231519bc7cfe40d2bab08d07eaff1c74b
ZymanteL <zymantelelesiute@gmail.com>
2023-03-09 21:04:24 +0200

    Changed task-10-javascript-prototypes.js

--- task-10-javascript-prototypes.js
+++ task-10-javascript-prototypes.js
@@ -1,26 +1,25 @@
-function  Book(name, author, pages) {
-    this.name = name;
-    this.author = author;
-    this.pages = pages;
+function Book(name, author, pages) {
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
 
-    this.howManypages = function () {
-        console.log("This book has" + this.pages);
-    }
+  this.howManypages = function () {
+    console.log("This book has" + this.pages);
+  };
 
-    this.whoWritenthis = function () {
-        console.log("This book is writen by" + this.author);
-    }
-
+  this.whoWritenthis = function () {
+    console.log("This book is writen by" + this.author);
+  };
 }
 
-var book1 = new Book("Murder on the Orient Express", "Agatha Christie", 256 )
-book1.whoWritenthis
+var book1 = new Book("Murder on the Orient Express", "Agatha Christie", 256);
+book1.whoWritenthis;
 
 function SummerRead() {
-    Book.apply(this. arguments)
+  Book.apply(this.arguments);
 
-    this.howManyDaysToRead= () => console.log(this.howManypages/20); 
-
+  this.howManyDaysToRead = () => console.log(this.howManypages / 20);
 }
 
-var book2 = new SummerRead("Philosopher's Stone", "J. K. Rowling", 223)
+var book2 = new SummerRead("Philosopher's Stone", "J. K. Rowling", 223);
+book2.howManyDaysToRead;
