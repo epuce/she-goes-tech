38924c09dd8e17467fcda58161ca1053a4dd4af9
Valda Naujokaitiene <valdanaujokaitiene@Valdas-Air.lan>
2023-03-12 16:28:11 +0200

    JS prototypes

--- CRUD.js
+++ CRUD.js
@@ -1,54 +1,61 @@
 var nameInput = document.querySelector(".js-name");
 var emailInput = document.querySelector(".js-email");
 
-function saveForm() {
-  var isFormValid = true;
+function User(name, email) {
+  this.email = typeof email === "string" ? email: " ";
+  // if (typeof name === "string") {this.name = name} else {this.name = "";}
 
-  if (nameInput.value.length < 3) {
-    nameInput.classList.add("validation-error");
-    isFormValid = false;
-  }
-  // add validation and error style. After defining "validaition-eror"
-  else {
-    nameInput.classList.remove("validation-error")
-    // remove the error style if the value is valid
-  }
-
+ 
   var emailRegEx = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  // use RegEx for validation codes.
 
-  if (!emailRegEx.test(emailInput.value)) {
-    // how we test the email.
-    // added ! makes the value the other way around.
-    emailInput.classList.add("validation-error")
-    isFormValid = false
-  }
-  else {
-    emailInput.classList.remove("validation-error")
+  this.setName = function(newName) {
+    this.name = typeof newName === "string" ? newName : "";
   }
 
-  if (isFormValid) {
-    var user = {
-      name: nameInput.value,
-      email: emailInput.value,
-    };
-    // 1st name that we specify as the key (we think of), 2nd - we have to reference.
+  this.setEmail = function(newEmail) {
+    this.email = typeof newEmail === "string" ? newEmail : "";
+  }
 
-    try {
-      var list = JSON.parse(localStorage.userlist);
-    } catch {
-      var list = getUserList();
-      // replaced bu function
+  this.isValidEmail = function() {
+    return emailRegEx.test(this.email)
+  }
+
+  this.isValidName = function() {
+    return nameInput.value.length >= 3
+  }
+
+  this.isValidUser = function() {
+    return this.isValidEmail() && this.isValidName()
+  }
+
+  this.getUser = function() {
+    return {
+      name: this.name,
+      email: this.email,
     }
-    // TRY to make a list, if not, make new one.
+  }
+
+  this.setName(name);
+  this.setEmail(email);
+}
+
+function saveForm() {
+  var user = new User(nameInput.value, emailInput.value)
+  // creating an instance with new parametres
+
+  nameInput.classList.toggle("validation-error", !user.isValidName())
+  emailInput.classList.toggle("validation-error", !user.isValidEmail())
+
+  if (user.isValidUser()) {
+    var list = getUserList()
 
     if (saveBtn.dataset.index) {
-      list [saveBtn.dataset.index] = user;
+      list [saveBtn.dataset.index] = user.getUser();
 
       delete saveBtn.dataset.index
       // saveBtn.dataset.index = " " - deleting
     } else {
-    list.push(user)
+    list.push(user.getUser())
     }
     // add a new user in the list. if - updatetes and existing one
 
@@ -101,17 +108,18 @@ function renderTable() {
   }
 
   list.forEach(function (user, index) {
+    var userInstance = new User(user.name, user.email);
     var row =
       `
         <tr>
             <td>` +
-      index +
+      (index+1)+
       `</td>
             <td>` +
-      user.name +
+      userInstance.name +
       `</td>
             <td>` +
-      user.email +
+      userInstance  .email +
       `</td>
       <td>
         <button class="js-edit" 
@@ -153,6 +161,11 @@ function renderTable() {
       var list = getUserList()
       var user = list[button.dataset.index]
       // specify the specific element and index
+      
+      var userInstance = new User()
+      userInstance.setEmail(user.email)
+      userInstance.setName(user.name)
+
       nameInput.value = user.name
       emailInput.value = user.email
 
---
+++ task-10-javascript-prototypes.js
@@ -0,0 +1,43 @@
+1.
+var animals = [dog, cat, mouse];
+var animals = ["dog", "cat", "mouse"];
+var count = animals.push("bird");
+console.log(animals)
+---
+2.
+var Info = {
+    a: 'green',
+    b: 42,
+    c: false
+  };
+  console.log(Object.values(Info))
+---
+3.
+function Person(name, age, favSnack) {
+    this.name = name
+    this.age = age
+    this.favSnack = favSnack
+
+    this.sayHi = function() {
+        return "Hello, my name is " + this.name
+    }
+}
+var Person = new Person("Tim", 23, "apple")
+
+---
+function Person(name, age, favSnack) {
+    this.name = name
+    if (typeof age === "number") {
+        this.age = age} 
+    else {
+        this.age = ""}
+    this.favSnack = favSnack
+
+    this.sayHi = function() {
+        return "Hello, my name is " + this.name
+    }
+}
+---
+function Admin() {
+    Person.apply(this, arguments)
+}
