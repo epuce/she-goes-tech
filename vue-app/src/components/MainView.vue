e3b128e55cae83d7a29739b531a55026a98d692e
Ieva Zvanitaja Rubine <ieva.zvanitaja.rubine@gmail.com>
2023-03-21 21:32:56 +0200

    vue

---
+++ vue-app/src/components/MainView.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        Yay it works
+
+        <h1>{{ heading }}</h1>
+        <h2>You have clicked {{ clickCount }} times</h2>
+
+        <!-- <button @click="addCount">Add count</button> -->
+        <!-- we are adding an eventListener with @click="addCount" -->
+        <MyBtn text="Add count" :type="btnType"></MyBtn> 
+        <!-- a column : befor the type mena swe reference a variable -->
+    </div>
+
+</template>
+<script>
+import{defineComponent, ref} from 'vue';
+import MyBtn from './MyBtn.vue';
+
+export default defineComponent({
+    components: {
+        MyBtn: MyBtn
+    },
+    props: { /*adding custom parameters*/
+        heading: {
+            type: String,
+            required: false,
+            default: 'Vue data binding works'
+
+           /*required: true,
+            we can also set the required to 'true' and then we don't need a default value*/
+        },
+        btnType: {
+            type: String,
+            required: false,
+            default: undefined
+        }
+    },
+    setup() {
+        var clickCount = ref(0) /*ref alows us to have reactivity - if something changes in javascript, then it also changes in the HTML*/
+
+        var addCount = function() {
+            clickCount.value = clickCount.value + 1
+        }
+
+        return {
+            clickCount: clickCount,
+            addCount: addCount,
+        }
+    }
+})
+</script>
+
+<style>
+
+</style>
---
+++ vue-app/src/components/MyBtn.vue
@@ -0,0 +1,47 @@
+<template>
+    <button @click="onClick()"
+            :class="{
+                'btn--primary': type === 'primary', /*if button type is primary, we add the class primary*/
+                'btn--warning': type === 'warning' /*we add class waring, if button type is waring*/
+                /*or we can do it like this
+                `'btn--' + type` - her we use string contatination, to get the necessary class*/
+                }"
+            >
+        {{ text }}</button>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent ({
+    props: {
+        text: {
+            type: String,
+            required: true,
+        },
+        type: {
+            type: String,
+            required: false,
+            default: "primary"
+        }
+    },
+    setup() {
+
+        var onClick = function () {
+
+        }
+        return {
+            onClick: onClick
+        }
+    }
+})
+</script>
+<style>
+.btn--primary {
+    background: blue;
+}
+
+.btn--warning {
+    background: orange;
+}
+</style>
