debb585f301e62fb37a26a724731a282516f7b2a
KristePe <biskyte@hotmail.com>
2023-03-21 22:09:55 +0200

    vue-app added

---
+++ vue-app/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <MainView heading="some cool text" btnType="warning"/>
+  <MainView heading="this also works"/>
+  <MainView/>
+</template>
+
+<script>
+
+import MainView from './components/MainView.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MainView
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-app/src/components/MainView.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    Yay it works
+
+    <h1>{{ heading }}</h1>
+    <h2>You have clicked {{ clickCount }} times</h2>
+    <!-- <button @click="addCount">Add count</button> -->
+    <MyBtn text="Add count" :type="btnType" />
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+import MyBtn from "./MyBtn.vue";
+
+export default defineComponent({
+  components: {
+    MyBtn: MyBtn,
+  },
+  props: {
+    heading: {
+      type: String,
+      required: false,
+      default: "Vue data binding works",
+    },
+    btnType: {
+      type: String,
+      required: false,
+      default: undefined,
+    },
+  },
+  setup() {
+    // var heading = "Vue data binding works"
+    var clickCount = ref(0);
+    var addCount = function () {
+      clickCount.value = clickCount.value + 1;
+    };
+    return {
+      clickCount: clickCount,
+      addCount: addCount,
+    };
+  },
+});
+</script>
+
+<style></style>
