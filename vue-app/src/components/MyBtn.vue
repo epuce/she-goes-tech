bcde8497197fc3011151d0d08221095ee0955837
Valda Naujokaitiene <valdanaujokaitiene@Valdas-Air.lan>
2023-03-24 15:25:17 +0200

    vue app

---
+++ vue-app/src/components/MyBtn.vue
@@ -0,0 +1,32 @@
+<template>
+    <button :class="{
+        'btn--primary': type === 'primary'
+    }">
+        {{ text }}
+    </button>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent ({
+    props: {
+        text: {
+            type: String,
+            required: true,
+        },
+        type: {
+            type: String,
+            required: false,
+            default: 'primary'
+        },
+    },
+})
+</script>
+<style>
+.btn--primary {
+    background: blue;
+}
+.btn--warning {
+    background: orange;
+}
+</style>
---
+++ vue-app/src/components/TaskVueIntro.vue
@@ -0,0 +1,34 @@
+<template>
+    <button class="btn__change btn__clic" @click= "changeButtonText()"> Click me </button>
+
+    <button class='btn__open' @click= "onClick()"> TEXT </button>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+
+    props: {
+        text: {  
+            type: String,
+            required: true,
+        }
+    },
+    setup() {
+        var changeButtonText = function(){
+                this.buttonText = "Clicked"
+            }
+
+        return {
+            changeButtonText: changeButtonText
+        
+        }
+    }
+    
+
+
+})
+
+</script>
+<style></style>
