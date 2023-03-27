942bbfd9649fcc29a61d8819e6ad797e198b5cb8
Agne <agne.buraityte@gmail.com>
2023-03-27 18:07:17 +0300

    add vue files

---
+++ vue-app/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <MainView heading="some cool text" btnType="warning" />
+    <!-- items are reusable, but not related to each other. Props are treated as attributes -->
+    <MainView heading="this also works" />
+    <MainView />
+  </div>
+</template>
+
+<script>
+import MainView from "./components/MainView.vue"; // switch from hello world, to the one we are using now
+
+export default {
+  name: "App",
+  components: {
+    MainView,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
