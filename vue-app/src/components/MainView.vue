d0b73afd126728276df3f3dc13ae0ee19ebb8abf
lindalejiete <116344082+lindalejiete@users.noreply.github.com>
2023-03-27 18:22:51 +0300

    Vue project created

---
+++ vue-app/src/components/MainView.vue
@@ -0,0 +1,73 @@
+<template>
+    <div>
+        Yay it works
+
+        <h1>{{ heading }}</h1>
+        <h2>You have clicked {{ clickCount }} times</h2>
+
+        <!-- <button @click="addCount()">Add count</button> -->
+        <MyBtn text="Add count" @click="addCount()" :type="btnType" something-else="this is some text" />
+        <MyBtn text="Content" @click="addCount()">        </MyBtn>
+        <button @click="isOpen = true">Open popup</button>
+        <input v-model="someText">
+        
+        {{ someText }}
+
+        <TaskVueIntro></TaskVueIntro>
+
+        <MyPopup 
+            :text="someText"
+            v-if="isOpen" 
+            @close-popup="isOpen = false"/>
+
+    </div>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue';
+import MyBtn from './MyBtn.vue';
+import MyPopup from './MyPopup.vue';
+import TaskVueIntro from './TaskVueIntro.vue';
+
+export default defineComponent({
+    components: {
+        MyBtn: MyBtn,
+        MyPopup,
+        TaskVueIntro,
+       
+    },
+    props: {
+        heading: {
+            type: String,
+            required: false,
+            default: "Vue data binding works"
+        },
+        btnType: {
+            type: String,
+            required: false,
+            default: undefined,
+        }
+    },
+    setup() {
+        // var heading = "Vue data binding works"
+        var clickCount = ref(0)
+        var isOpen = ref(false)
+        var someText = ref('')
+
+        var addCount = function() {
+            clickCount.value = clickCount.value+1
+        }
+        
+        return {
+            clickCount: clickCount,
+            addCount: addCount,
+            isOpen,
+            someText,
+            isActive: false,
+        }
+    },
+})
+</script>
+
+<style>
+</style>
---
+++ vue-app/src/components/MyPopup2.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="popup__wrapper">
+        <div class="popup">
+            {{ popupText }}
+            <button class="btn_close" @click="onClose()">Close</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        props: {
+            text: {
+                type: String,
+                required: true,
+            },
+            popupText: {
+                type: String,
+                required: false,
+                default: "You did it"
+        }
+        },
+        setup(props, { emit }) {
+            var onClose = function() {
+                emit('close-popup')
+            }
+
+            return {
+                onClose
+            }
+        }
+    })
+</script>
+
+<style>
+.popup__wrapper {
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: rgba(0,0,0,0.3);
+}
+
+.popup {
+    padding: 32px;
+    width: 30%;
+    background: #cfcfcf;
+    border-radius: 10px;
+}
+
+.btn_close {
+    float: right;
+}
+
+.closed {
+    background: red;
+}
+
+</style>
