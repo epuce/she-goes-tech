56b0e184169d854baa1ac86ec1fef6643d658ab9
KristePe <biskyte@hotmail.com>
2023-03-30 22:07:58 +0300

    routers updates

---
+++ vue-app/src/components/ListView.vue
@@ -0,0 +1,12 @@
+<template lang="">
+    List view
+</template>
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+    
+})
+</script>
+<style lang="">
+    
+</style>
--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -1,7 +1,31 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import * as VueRouter from 'vue-router'
 
+var router = VueRouter.createRouter({
+    history: VueRouter.createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            component: function() {
+                return import('./components/MainView.vue')
+            }
+        },
+        {
+            path: '/list',
+            component: function() {
+                return import('./components/ListView.vue')
+            }
+        },
+        {
+            path: '/:pathMatch(.*)*',
+            redirect: '/'
+        }
+    ]
+})
 
- 
+var app = createApp(App)
 
-createApp(App).mount('#app')
+app.use(router)
+
+app.mount('#app')
