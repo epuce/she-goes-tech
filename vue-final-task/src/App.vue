2896646c2c902c2885d94ea7698a3f35ea1429d2
Ieva Zvanitaja Rubine <ieva.zvanitaja.rubine@gmail.com>
2023-03-31 23:02:39 +0200

    vue

---
+++ vue-final-task/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <MainView/>
+</template>
+
+<script>
+import MainView from './components/MainView.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainView
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-final-task/src/components/MainView.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="main-view-wrapper">
+        <div class="form-view-warpper">
+            Form view
+        </div>
+        <div class="table-view-wrapper">
+            Table view
+        </div>
+        <div class="popup-overlay">
+
+        </div>
+    </div>
+</template>
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent ({ 
+    
+})
+</script>
+<style>
+body {
+    margin: 0;
+}
+.main-view-wrapper {
+    display: flex;
+}
+
+.table-view-wrapper {
+    display: flex;
+    width: 100%;
+    justify-content: center;
+}
+
+.popup-overlay {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(202, 196, 196, 0.815);
+}
+
+</style>
