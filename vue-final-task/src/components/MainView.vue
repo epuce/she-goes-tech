0818c37f9eb6d71c67b1624650fedca36867a4d7
Ieva Zvanitaja Rubine <ieva.zvanitaja.rubine@gmail.com>
2023-04-03 01:00:09 +0200

    vue-final-task

--- vue-final-task/src/components/MainView.vue
+++ vue-final-task/src/components/MainView.vue
@@ -1,21 +1,28 @@
 <template>
     <div class="main-view-wrapper">
-        <div class="form-view-warpper">
+        <OpenFormBtn/>
+        <!-- <div class="form-view-warpper">
             Form view
-        </div>
+        </div> -->
         <div class="table-view-wrapper">
             Table view
         </div>
-        <div class="popup-overlay">
-
-        </div>
+        <!-- <div class="popup-overlay">
+            <div class="popup">
+                <div class="text">This is a popup</div>
+                <button>x</button>
+            </div>
+        </div> -->
     </div>
 </template>
 <script>
 import { defineComponent } from 'vue';
+import OpenFormBtn from './OpenFormBtn.vue'
 
 export default defineComponent ({ 
-    
+    components: {
+    OpenFormBtn
+  },
 })
 </script>
 <style>
@@ -33,10 +40,34 @@ body {
 }
 
 .popup-overlay {
+    display: flex;
+    justify-content: center;
+    align-items: center;
     position: absolute;
     width: 100%;
     height: 100%;
     background-color: rgba(202, 196, 196, 0.815);
+}
+
+.popup {
+    position:relative;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    width: 300px;
+    height: 100 px;
+    background-color: grey;
+    padding: 30px;
+    justify-content: center;
+    align-items: center;
+}
+
+.text {
+    display: block;
+}
+
+button {
+    display:inline-block;
 }
 
 </style>
---
+++ vue-final-task/src/components/OpenFormBtn.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <button type="button" class="open-form__btn" @click="openBtn" :class="isBtnOpened ? 'open-form__btn--opened' : ''"><span>Open Form</span></button>
+    </div>
+</template>
+<script>
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+    setup () {
+        var isBtnOpened = ref(false);
+
+        var openBtn = function () {
+            isBtnOpened.value = !isBtnOpened.value
+        }
+
+        return {
+            isBtnOpened,
+            openBtn
+        }
+    }
+
+
+})
+</script>
+<style>
+.open-form__btn {
+    background-color: darkred;
+    width: 30px;
+    height: 90px;
+    position: fixed;
+    top: 30%;
+    left: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: solid 1px darkred;
+    border-radius: 0 5px 5px 0;
+    cursor: pointer;
+    transition: left 1s ease-in-out;
+}
+
+.open-form__btn span {
+    transform: rotate(90deg);
+    white-space: nowrap;
+    font-weight: 700;
+}
+
+.open-form__btn--opened {
+    left: 200px;
+    
+}
+</style>
