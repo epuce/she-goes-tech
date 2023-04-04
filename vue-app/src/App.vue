c1ddffe263e16a547462094fd102a7a79136cac8
lindalejiete <116344082+lindalejiete@users.noreply.github.com>
2023-04-04 18:06:49 +0300

    update

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,77 +1,79 @@
 <template>
   <div>
-    <MainView heading="Some cool text" btnType="warning"/>
-    <MainView heading="This also works"/>
-    <MainView/>
-    <UserSelect :users="userList"/>
+   <MainView heading="Some cool text" btnType="warning" />
+   <MainView heading="This also works"/>
+   <MainView/>
+    <!-- <UserSelect :users="userList"/>
 
-    <router-link to="/">Main</router-link>
-    <br />
-    <router-link to="/list">List</router-link>
+    <router-link to="/" >Main</router-link>
+      <br />
+      <router-link to="/list" >List</router-link>
+      <br />
+      <router-link to="/something" >Something</router-link>
 
-    <router-view></router-view>
+      <router-view></router-view>
 
-    <UserView />
+      <UserView />-->
   </div>
 </template>
 
+<!-- App.vue(btnType) -> MainView.vue(type) -> MyBtn.vue -->
+
 <script>
 import MainView from './components/MainView.vue'
-
-import UserSelect from './components/UserSelect/UserSelect.vue';
-import UserView from './components/UserView.vue';
-
+//import UserSelect from './components/UserSelect/UserSelect.vue'
+//import UserView from './components/UserView.vue';
 export default {
   name: 'App',
   components: {
     MainView,
-    UserSelect,
-    UserView
+    //UserSelect,
+    //UserView
   },
   methods: {
-   
   },
-  /*data() {
+  data() {
     var userList = [
       {
         id: 1,
-        avatar: 'https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=2000',
-        name: 'Jim Jimmies',
+        avatar: 'https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg',
+        name: 'Jam Jimmies',
       },
       {
         id: 7,
-        avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSla-p7MqWVSLL2rpSQHlxEO6mKceKYPvZjo4oslefoeXE7-oMcRHP5IfT3qgllHC8kKvQ&usqp=CAU',
-        name: 'Jack Daniels',
-      },
+        avatar: 'https://www.w3schools.com/howto/img_avatar.png',
+        name: 'Jack Jimmies',
+      },      
       {
         id: 10,
-        avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFWBnyvJtrWYVL5q2Nwl1UkCOxOPxFGoORjg&usqp=CAU',
-        name: 'Jil Jimmies',
-      },
-      {
-        id: 23,
-        avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQn8P6POnmXE2YJlFMqlJ-b2F_t8bdqTq4CAb-mQWDeI813MCCXefNOg9RjN2AQZwPzy3Y&usqp=CAU',
-        name: 'Andy Jimmies',
+        avatar: 'https://www.w3schools.com/howto/img_avatar2.png',
+        name: 'Jamson Jimmies',
       },
       {
         id: 2,
-        avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_tQh_tPf03EMGr93KsBinmgw3nm1sqnwr51fFN4nt7bgwQwfWgQ1sMh8qzEbboM7rYWg&usqp=CAU',
+        avatar: 'https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg',
+        name: 'Andy',
+      },      
+      {
+        id: 5,
+        avatar: 'https://www.w3schools.com/howto/img_avatar.png',
         name: 'Johnie Jimmies',
       },
     ];
-
-    var selectedUserIds = JSON.parse(localStorage.selectedUserIds)
-
+    var selectedUserIds;
+    try {
+      selectedUserIds = JSON.parse(localStorage.selectedUserIds)
+    } catch {
+      selectedUserIds = []
+    }
     userList = userList.map(function(user) {
       user.isSelected = selectedUserIds.includes(user.id)
-
       return user;
     })
-
     return {
       userList
     }
-  }*/
+  }
 }
 </script>
 
--- vue-app/src/components/MainView.vue
+++ vue-app/src/components/MainView.vue
@@ -8,20 +8,18 @@
         <!-- <button @click="addCount()">Add count</button> -->
         <MyBtn text="Add count" @click="addCount()" :type="btnType" something-else="this is some text" />
         <MyBtn text="Content" @click="addCount()">        </MyBtn>
-        <button @click="isOpen = true">Open popup</button>
+
+        <button @click="isOpen = true" :class="{'color-red': changeClass}">Open popup</button>
         <input v-model="someText">
         
         {{ someText }}
 
-        <TaskVueIntro></TaskVueIntro>
-
         <MyPopup 
             :text="someText"
             v-if="isOpen" 
-            @close-popup="isOpen = false"/>
+            @close-popup="onClose()"/>
 
         <TaskServerRequest></TaskServerRequest>
-
     </div>
 </template>
 
@@ -29,16 +27,13 @@
 import { defineComponent, ref } from 'vue';
 import MyBtn from './MyBtn.vue';
 import MyPopup from './MyPopup.vue';
-import TaskVueIntro from './TaskVueIntro.vue';
 import TaskServerRequest from './TaskServerRequest.vue';
-
 export default defineComponent({
     components: {
-    MyBtn: MyBtn,
-    MyPopup,
-    TaskVueIntro,
-    TaskServerRequest,
-},
+        MyBtn: MyBtn,
+        MyPopup,
+        TaskServerRequest,
+    },
     props: {
         heading: {
             type: String,
@@ -49,26 +44,30 @@ export default defineComponent({
             type: String,
             required: false,
             default: undefined,
-        }
+        } 
     },
     setup() {
         // var heading = "Vue data binding works"
         var clickCount = ref(0)
         var isOpen = ref(false)
         var someText = ref('')
-
+        var changeClass = ref(false)
         var addCount = function() {
             clickCount.value = clickCount.value+1
         }
-        
+        var onClose = function(){
+            isOpen.value = false;
+            changeClass.value = true;
+        }
         return {
             clickCount: clickCount,
             addCount: addCount,
             isOpen,
             someText,
-            isActive: false,
+            onClose,
+            changeClass,
         }
-    },
+    }
 })
 </script>
 
