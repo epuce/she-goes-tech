3efc27d30a54e5956d6912b4867269d3a6177a9f
Valda Naujokaitiene <valdanaujokaitiene@Valdas-Air.lan>
2023-04-04 21:32:36 +0300

    node

---
+++ nodejs/routes/user.route.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var router = express.Router()
+
+// /api/users -> GET
+router.get('', function(_, response) {
+    response.send('Will send userlist')
+})
+
+// /api/users -> /:id  -> GET
+router.get('/:id', function(request, response) {
+    response.send('Will send an user with id: ' + request.params.id)
+})
+
+// /api/users -> /:id -> DELETE
+// /api/users -> /:id -> PUT
+// /api/users/ -> POST
+
+
+module.exports = router
