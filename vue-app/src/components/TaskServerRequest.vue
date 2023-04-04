a12b80c62073ce4677ce979b2265a9c5fa5ba226
Valda Naujokaitiene <valdanaujokaitiene@Valdas-Air.lan>
2023-04-04 14:57:26 +0300

    Fetch task

--- vue-app/src/components/TaskServerRequest.vue
+++ vue-app/src/components/TaskServerRequest.vue
@@ -1,18 +1,57 @@
 <template lang="">
-    <div>
-        
+    <div class='grid-wrapper'>
+        <div v-for='(user) in users' :key='user.id'>
+            <div> 
+                <TaskImage :task_image="user.image" />
+            </div>
+            <div class='user-name'> {{user.firstname}} {{user.lastname}}</div>
+            <div class='user-email'> {{user.email}} </div>
+        </div>
     </div>
 </template>
 
 <script>
 
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
+import TaskImage from './UserSelect/TaskImage.vue';
 
 export default defineComponent ({
-    
+    components: {
+        TaskImage,
+    },
+
+    setup() {
+        var users = ref([])
+        fetch('https://fakerapi.it/api/v1/persons', {
+            method: 'GET'
+        }).then(function(response) {
+            return response.json()
+        }).then(function(returnData) {
+            users.value = returnData.data
+        })
+        
+
+        return {
+        users,
+        TaskImage
+        }
+    }
 })
 
 </script>
-<style lang="">
+<style>
+
+.grid-wrapper {
+    display: grid;
+    width: 100%;
+    height: 500px;
+    grid-template-columns: 25% 25% 25% 25% 25%;
+    grid-template-rows: (25%, 25%);
+
+}
+
+.user-name {
+
+}
     
 </style>
