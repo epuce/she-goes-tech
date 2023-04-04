d808e341b8b17788ae606aed27bb7201e627fd56
KristePe <biskyte@hotmail.com>
2023-04-04 11:19:22 +0300

    styling updates

--- vue-app/src/components/TaskServerRequest.vue
+++ vue-app/src/components/TaskServerRequest.vue
@@ -1,52 +1,60 @@
-<template lang="">
-    <div>
-        task server request
-<table>
-    <tbody>
-<tr v-for="user in users" :key="user.id">
-    <td>{{user.image}}</td>
-    <td>{{user.firstname}} {{user.lastname}}</td>
-    <td>{{user.email}}</td>
-</tr>
-      
-
-    </tbody>
-</table>
-
+<template>
+  <div>
+    <div class="main-container" >
+      <div class="user-card" v-for="user in users" :key="user.id">
+        <img :src="user.image" class="imgPicture" alt="" />
+          <h3>{{ user.firstname }} {{ user.lastname }}</h3>
+          <p>{{ user.email }}</p>
+      </div>
     </div>
+  </div>
 </template>
 <script>
-import { defineComponent, ref } from 'vue';
+import { defineComponent, ref } from "vue";
 export default defineComponent({
-    
-    setup() {
-        var users = ref([]);
-        var user = ref({
-            name: "",
-            email: "",
-            image: "",
-        })
+  setup() {
+    var users = ref([]);
+    var user = ref({
+      name: "",
+      email: "",
+      image: "",
+    });
 
-        fetch(
-            "https://fakerapi.it/api/v1/persons", {
-                method: "GET"
-            }
-        ).then(function(response){
-            return response.json()
-        }).then(function(returnData){
-            users.value = returnData.data 
-        })
+    fetch("https://fakerapi.it/api/v1/persons", {
+      method: "GET",
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (returnData) {
+        users.value = returnData.data;
+      });
 
-        return {
-            users,
-            user
-        }
-
-    }
-
-
-})
+    return {
+      users,
+      user,
+    };
+  },
+});
 </script>
-<style lang="">
-    
+<style scoped>
+.main-container {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+}
+.user-card {
+    font-size: 0.7rem;
+    margin: 10px;
+    box-sizing: border-box;
+    text-align: start;
+    flex-direction: column;
+}
+.imgPicture {
+  height: 150px;
+}
+h3, p {
+    margin: 2px;
+}
+
 </style>
