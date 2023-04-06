3628a23916f07fde082ce8ca2ba59c2258d786f0
Edmunds Puce <i-am-funny@yahoo.com>
2023-04-06 20:27:22 +0300

    SQL intro

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,48 +1,41 @@
 var express = require('express')
+var cors = require('cors')
+var mysql = require('mysql')
+var userRoutes = require('./routes/user.route')
+var sqlRoutes = require('./routes/sql.route')
 
 var app = new express()
+app.use(express.json())
+// app.use(cors)
 
 var router = express.Router()
-
-router.get('/api/users', function(r, response) {
-    var users = [
-        {
-          id: 1,
-          avatar: 'https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg',
-          name: 'Jam Jimmies',
-        },
-        {
-          id: 7,
-          avatar: 'https://www.w3schools.com/howto/img_avatar.png',
-          name: 'Jack Jimmies',
-        },      
-        {
-          id: 10,
-          avatar: 'https://www.w3schools.com/howto/img_avatar2.png',
-          name: 'Jamson Jimmies',
-        },
-        {
-          id: 2,
-          avatar: 'https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg',
-          name: 'Andy',
-        },      
-        {
-          id: 5,
-          avatar: 'https://www.w3schools.com/howto/img_avatar.png',
-          name: 'Johnie Jimmies',
-        },
-    ];
-
-    response.send(JSON.stringify(users))
-})
 
 router.get('*', function(reject, response) {
     console.log("Response was sent to the browser")
     response.send("You accessed the node server")
 })
 
+app.use('/api/users', userRoutes)
+app.use('/api/sql', sqlRoutes)
 app.use(router)
+
+var db = mysql.createConnection({
+    host: '104.248.125.41',
+    user: 'user-she-goes-tech',
+    password: 'password-she-goes-tech',
+    database: 'she-goes-tech',
+})
+
+db.connect(function(error) {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log("Connected to database")
+    }
+})
 
 app.listen(8002, function() {
     console.log('Server is up on http://localhost:8002')
 })
+
+exports.db = db
