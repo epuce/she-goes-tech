940004ff637b7a2b1fe7a455b0c682c10e8a9605
LindaAlpeLuka <linda.selga@gmail.com>
2023-04-10 18:39:11 +0300

    added vue check-up tasks

---
+++ vue-app/src/components/TaskServerRequest.vue
@@ -0,0 +1,36 @@
+<template lang="">
+    <div>
+       <div v-for="(user) in users" :key="user.id">
+        <!-- <img :src="user.image" @click=expandingImg(i)> -->
+    {{user}}
+        </div>
+    </div>
+</template>
+<script>
+import { defineComponent, ref } from 'vue';
+export default defineComponent ({
+    setup() {
+        var users = ref([])
+
+        fetch ("https://fakerapi.it/api/v1/persons", {
+            method: "GET"
+        }).then(function (response) {
+                return response.json()
+            }).then(function (returnData) {
+                users.value = returnData.data
+
+            })
+
+        // var expandingImg = function(i) {
+
+        // }
+
+    return {
+        users,
+    }
+    }
+})
+</script>
+<style lang="">
+    
+</style>
---
+++ vue-app/src/components/TaskVueIntro.vue
@@ -0,0 +1,55 @@
+<template>
+    <span>
+        <button v-on:click="changeText">{{ btnText }}</button>
+
+        <button :class="btnPopup" @click="isOpen=true">Open Popup</button>
+
+    <TaskVueIntroPopup v-if="isOpen" 
+                       text="You did it"
+                       @close-popup="isOpen=false; btnPopup='btn-clicked'"
+                       >
+    </TaskVueIntroPopup>
+</span>
+
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue';
+import TaskVueIntroPopup from './TaskVueIntroPopup.vue';
+
+export default defineComponent ({
+    components: { 
+        TaskVueIntroPopup, 
+    },
+    
+    props: {
+        
+    },
+    setup() {
+        var btnText = ref("Click me");
+        var changeText = function () {
+            this.btnText = "Clicked"
+        } 
+
+        var isOpen = ref(false)
+        var btnPopup = ref("")
+
+        
+        return {
+            btnText,
+            changeText,
+            isOpen,
+            btnPopup,
+        };
+    },
+    
+})
+
+</script>
+
+<style>
+
+.btn-clicked {
+    background: red;
+}
+</style>
