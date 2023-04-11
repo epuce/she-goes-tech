9328ef61d02087abbe4b32c848be8a4f28b22c15
Karolina Masharo <karolinamasharo@gmail.com>
2023-04-11 20:09:26 +0300

    Add task 13

---
+++ task-13-SQL-basics.sql
@@ -0,0 +1,20 @@
+-- Create table
+CREATE TABLE IF NOT EXISTS `moharso-comments` (
+    id INT NOT NULL AUTO_INCREMENT, 
+    comment VARCHAR (800), 
+    user_id INT, 
+    PRIMARY KEY (id)
+    )
+
+-- Insert 3 rows
+INSERT INTO 'moharso-comments' (comment, user_id) VALUES (comm, 1), (comm, 2), (comm, 3)
+
+-- Retrievieng data
+SELECT * FROM `moharso-comments`
+
+-- Update 
+UPDATE `moharso-comments` SET comment="New comm", user_id="55" WHERE id=1
+
+-- Delete
+DELETE FROM `Comments` WHERE id=3
+
