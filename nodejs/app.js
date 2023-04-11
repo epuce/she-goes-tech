ed3839a98ebdb11dcd4af1d0753e6304aecf0edf
Vaida <linkuviene.vaida@gmail.com>
2023-04-11 07:17:57 +0300

    created table in nodejs files

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,38 +1,39 @@
 var express = require('express');
-var mysql = require('mysql');
-var cors = require('cors');
-var userRoutes = require('./routes/user.route');
-
 var app = new express();
-app.use(express.json())
-// app.use(cors());
+var cors = require('cors');
+var mysql = require('mysql');
+var userRoutes = require('./routes/user.route');
+var sqlRoutes = require('./routes/sql.route')
 
-var router = express.Router();
+app.use(express.json());
+// app.use(cors);
 
-router.get('*', function(reject, response) {
-    console.log('Response was sent to the browser');
-    response.send("You accessed the node server");
+var router = express.Router()
+
+router.get('*', (reject, response) =>{
+    console.log("Response was sent to the browser")
+    response.send("You accessed the node server")
 })
 
 app.use('/api/users', userRoutes);
+app.use('/api/sql', sqlRoutes)
 app.use(router);
 
 var db = mysql.createConnection({
     host: '104.248.125.41',
     user: 'user-she-goes-tech',
     password: 'password-she-goes-tech',
-    database: 'she-goes-tech'
+    database: 'she-goes-tech',
 })
 
-db.connect((error) => {
-    if (error) {
-        console.log(error);
-    } else {
+db.connect((error)=>{
+    if(error){
+        console.log(error)
+    }else{
         console.log("Connected to database")
     }
 })
-
-app.listen(8002, () => {
+app.listen(8002, () =>{
     console.log('Server is up on http://localhost:8002')
 })
 
--- nodejs/routes/user.route.js
+++ nodejs/routes/user.route.js
@@ -1,16 +1,71 @@
-var express = require('express')
-var router = express.Router()
+var express = require('express');
+var router = express.Router();
+var app = require('../app')
 
-// api/users -> GET
-router.get('', function(_, response){
-    response.send("Will send user list")
+function runSql(sql, response){
+    app.db.query(sql, (error, data)=>{
+        var returnData ={}
+        if(error){
+            returnData.error = error;
+        }else{
+            returnData.data = data
+        }
+        response.send(JSON.stringify(returnData))
+    })
+}
+
+// /api/users -> GET
+router.get('', (_, response)=>{
+    var sql = 'SELECT id, first_name AS name, CONCAT(first_name, " ", last_name) AS full_name FROM `VaidaLinkuviene-users`'
+    runSql(sql, response)
 })
-// api/users/id -> /:id -> GET
-router.get('/:id', function(request, response){
-    response.send("Will send an user with id: " + request.params.id)
+//  /api/users -> /:id -> GET
+router.get('/:id', (request, response) => {
+    var sql = 'SELECT * FROM `VaidaLinkuviene-users` WHERE id='+request.params.id
+    runSql(sql, response)
 })
-// api/users/id -> /:id -> DELETE
-// api/users/id -> /:id -> PUT
-// api/users -> POST
+// /api/users -> /:id -> DELETE
+    router.delete('/:id', (request, response)=>{
+        var sql = 'DELETE FROM `VaidaLinkuviene-users` WHERE id=' +request.params.id;
+        runSql(sql, response)
+    })
+// /api/users -> /:id -> PUT
+router.put('/:id', (request, response)=>{
+    var{first_name, last_name, email} = request.body;
+
+    var changeValues='';
+    if(first_name){
+        changeValues += `first_name="`+first_name+`"`
+    }
+    if(last_name){
+        changeValues += `last_name="`+last_name+`"`
+    }
+    if(email){
+        changeValues += `email="`+email+`"`
+    }
+
+    var sql = `UPDATE \`VaidaLinkuviene-users\` 
+    SET first_name="`+first_name+`", last_name="`+last_name+`", email="`+email+`" WHERE id=`+request.params.id;
+    runSql(sql, response)
+})
+// /api/users -> POST
+router.post('', (request, response)=>{
+    var first_name = request.body.first_name;
+    var last_name = request.body.last_name;
+    var email = request.body.email;
+    // var {first_name, last_name, email} = request.body ;
+
+    var sql = `INSERT INTO \`VaidaLinkuviene-users\` (first_name, last_name, email)   VALUES(" `+first_name+`", "`+last_name+`", "`+email+`")`
+
+    app.db.query(sql, (error, data)=>{
+        var returnData ={}
+        if(error){
+        }else{
+            returnData.data = data
+        }
+        response.send(JSON.stringify(returnData))
+    })
+
+})
 
 module.exports = router
