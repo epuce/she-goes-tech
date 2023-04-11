442c7b2e2e38847a5bd5eb6515ade8fc4ce7439b
neringa1991 <neringa1991@yahoo.com>
2023-04-11 18:17:21 +0300

    add new files

--- nodejs/routes/user.route.js
+++ nodejs/routes/user.route.js
@@ -1,40 +1,56 @@
 // we will specify our routes
 var express = require("express");
 var router = express.Router();
+var app = require("../app");
+
+function runSql(sql, response) {
+  app.db.query(sql, function (error, data) {
+    var returnData = {};
+
+    if (error) {
+      returnData.error = error;
+    } else {
+      returnData.data = data;
+    }
+
+    response.send(JSON.stringify(returnData));
+  });
+}
 
 // here we need to specify ids and the methods
 // /api/users-->with GET method
 // to read something
 router.get("", function (_, response) {
-  var sql =
-    'SELECT id, first_name AS name, CONCAT(first_name, "", last_name) AS full_name FROM `neringa-nedzinskiene`';
+  // * means we select the all the columns in the table we created
+  var sql = "SELECT * FROM `neringa1991-users`";
+
+  // below will allow to get only id and first_name
+  // var sql = "SELECT id, first_name FROM `neringa1991-users`";
+
+  // returned object key will be changed from first_name to name
+  // var sql = "SELECT id, first_name AS name FROM `neringa1991-users`";
+
+  // we can concatinate two columns values and return one column full_name with concatinated value
+  // var sql =
+  //   'SELECT id, first_name AS name, CONCAT (first_name, " ",last_name) as full_name FROM `neringa1991-users`';
+
   runSql(sql, response);
 });
+
 // /api/users/:id-->with GET
 // we need request beause we will read an id
 router.get("/:id", function (request, response) {
-  var sql =
-    "SELECT*FROM `neringa-nedzinskiene-users` where id=" + request.params.id;
-
+  var sql = "SELECT * FROM `neringa1991-users` WHERE id=" + request.params.id;
   runSql(sql, response);
 });
 
 // /api/users/:id-->DELETE method
 // to delete something
 router.delete("/:id", function (request, response) {
-  var sql =
-    "DELETE FROM `neringa-nedzinskiene-users` WHERE id=" + request.params.id;
+  // if there was an user with ID and it was removed, that ID will not be given to any other user we will add in the future. Even if we would clean all the table, in the memory IDs would remain and wouldn't be used
+  var sql = "DELETE FROM `neringa1991-users` WHERE id=" + request.params.id;
   runSql(sql, response);
 });
-
-// router.delete("/:id", function (request, response) {
-//   response.send(
-//     "Will update user with id:  " +
-//       request.params.id +
-//       ". With data:" +
-//       JSON.stringify(request.body)
-//   );
-// });
 
 // /api/users/:id-->PUT
 // to update something
@@ -46,31 +62,36 @@ router.put("/:id", function (request, response) {
   //     JSON.stringify(request.body)
   // );
   var {first_name, last_name, email} = request.body;
+  // we can specify only a few keys, don't need to update the whole user
   var sql =
-    `UPDATE \'neringa-nedzinskiene-users\' SET first_name="` +
+    `UPDATE \`neringa1991-users\` 
+    SET first_name="` +
     first_name +
     `", last_name="` +
     last_name +
     `", email="` +
     email +
-    `" WHERE id=` +
+    `" 
+    WHERE id=` +
     request.params.id;
+  sql = sql.replace("\n", "");
 
   runSql(sql, response);
 });
 
 // /api/users -> POST
-// to create something
+// to create something e.g., data in empty table
 router.post("", function (request, response) {
-  var first_name = request.body.first_name;
-  var last_name = request.body.last_name;
-  var email = request.body.email;
+  // var first_name = request.body.first_name;
+  // var last_name = request.body.last_name;
+  // var email = request.body.email;
 
+  // the below one is exactly the same as the above 3 lines
   var {first_name, last_name, email} = request.body;
 
+  // script to add data
   var sql =
-    `
-        INSERT INTO \`edmunds.puce-users\`
+    `INSERT INTO \`neringa1991-users\`
         (first_name, last_name, email)
         VALUES("` +
     first_name +
