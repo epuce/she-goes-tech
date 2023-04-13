12300e05325cff14d5da77e13eaad19dd278a3dd
viola <pumpure@yahoo.com>
2023-04-12 23:31:24 -0600

    sql task

---
+++ task-13-SQL-basics.sql
@@ -0,0 +1,6 @@
+ var sql = `CREATE TABLE IF NOT EXISTS \`viola.pumpure-comments\` (
+    user_id VARCHAR(255), 
+    comment VARCHAR(255),
+    id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (id)
+    
