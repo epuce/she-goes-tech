0283aced841850c6f4f36715dbc0fd679efb93bd
lindalejiete <116344082+lindalejiete@users.noreply.github.com>
2023-04-18 21:32:05 +0300

    extra functionality added

--- new-vue-app/src/components/UserCommentsView.vue
+++ new-vue-app/src/components/UserCommentsView.vue
@@ -1,7 +1,30 @@
 
 <template>
     <div class="user-comments">
-        <div class="user-comments__comments"></div>
+        <div class="user-comments__comments">
+            <div class="user-comments__users">
+                <div v-for="user in userList"
+                    @click="onUserSelect(user.id)"
+                    :key="user.id"
+                    :title="user.first_name+' '+user.last_name"
+                    :class="{
+                        'user-comments__user-avatar--active': comments.user_id === user.id
+                    }"
+                    class="user-comments__user-avatar">
+
+                    {{ user.first_name[0] }} {{ user.last_name[0] }}
+                </div>
+            </div>
+            <div class="user-comments__textarea-wrapper" v-if="comments.user_id">
+                <textarea v-model="comments.data" class="user-comments__textarea"></textarea>
+                <button type="button" v-if="comments.data.length > 0" @click="onCommentSave()">Save</button>
+            </div>
+            <div class="comment-list">
+                <div v-for="item in comments.list" :key="item.id">
+                {{ item.comments }}
+                </div>
+            </div>
+        </div>
         <div class="user-comments__user">
             <form>
                 <label>
@@ -22,12 +45,19 @@
             </form>
 
             <div class="user-list">
-                <div v-for="user in userList" :key="user.id" class="user-list__user">
-                    {{ user.first_name }} {{ user.last_name }}
+                <div v-for="item in userList" 
+                    :key="item.id"
+                    class="user-list__user"
+                    :class="{
+                        'user-list__user--active': user.id === item.id
+                    }"
+                    @click="fillUserForm(item)">
+                    
+                    {{ item.first_name }} {{ item.last_name }}
 
-                    <font-awesome-icon 
+                    <img src="../assets/Trash-Icon-SVG-psofds.svg" 
                         icon="fa-trash"
-                        @click="onUserDelete(user.id)"
+                        @click.stop="onUserDelete(item.id)"
                         class="user-list__delete" />
                 </div>
             </div>
@@ -38,17 +68,25 @@
 import { defineComponent, ref } from 'vue';
 export default defineComponent({
     setup() {
+        const comments = ref({
+            user_id: null,
+            data: '',
+            list: []
+        })
+
         const user = ref({
             first_name: '',
             last_name: '',
             id: null
         })
+
         const userList = ref([])
         fetch('http://localhost:8002/api/users')
         .then(resp => resp.json())
         .then(resp => {
             userList.value = resp.data
         })
+
         const onUserSave = () => {
             const payload = {
                 first_name: user.value.first_name,
@@ -113,12 +151,48 @@ export default defineComponent({
         const fillUserForm = (tmpUser) => {
             user.value = {...tmpUser};
         }
+
+        const onUserSelect = (userId) => {
+            comments.value.user_id = userId
+            fetch(`http://localhost:8002/api/comments?user_id=${userId}`)
+            .then(resp => resp.json())
+            .then(resp => {
+                comments.value.list = resp.data
+            })
+        }
+
+        const onCommentSave = () => {
+            const payload = {
+                comments: comments.value.data,
+                user_id: comments.value.user_id
+            }
+            fetch(`http://localhost:8002/api/comments`, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(payload)
+            })
+            .then(resp => resp.json())
+            .then(resp => {
+                if (!resp.error) {
+                    comments.value.list.push({
+                        ...payload,
+                        id: resp.data.insertId
+                    })
+                }
+            })
+        }
+
         return {
             userList,
             onUserSave,
             user,
             onUserDelete,
-            fillUserForm
+            fillUserForm,
+            onUserSelect,
+            comments,
+            onCommentSave
         }
     }
     
@@ -156,7 +230,8 @@ export default defineComponent({
     margin-bottom: 8px;
     cursor: pointer; 
 }
-.user-list__user:hover {
+.user-list__user:hover,
+.user-list__user--active {
     background: rgba(33,33,144, 0.05);
 }
 .user-list__user:hover .user-list__delete {
@@ -169,5 +244,44 @@ export default defineComponent({
     display: none;
     float: right;
     margin-right: 2px;
+    height: 18px;
+}
+
+.user-comments__users {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 12px;
+}
+
+.user-comments__user-avatar {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 32px;
+    height: 32px;
+    border: 2px solid #fff;
+    border-radius: 50%;
+    cursor: pointer;
+}
+
+.user-comments__user-avatar--active {
+    background: rgba(0,0,100, 0.2);
+}
+
+.user-comments__textarea {
+    margin-top: 16px;
+    width: 100%;
+    height: 100px;
+    resize: none;
+}
+
+.user-comments__textarea-wrapper {
+    position: relative;
+}
+
+.user-comments__textarea-wrapper button {
+    position: absolute;
+    right: 8px;
+    bottom: 8px;
 }
 </style>
