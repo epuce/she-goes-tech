94ef890d1474e29ba41ef3334d58c7e62a22d163
viola <pumpure@yahoo.com>
2023-04-24 09:30:36 -0600

    page layout

--- final-task/src/App.vue
+++ final-task/src/App.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
-        <LandingPage />
+        <div class="left">
+            <LandingPage />
+        </div>
+
+        <div class="right">
+            <Table />
+        </div>
 
     </div>
 </template>
@@ -20,5 +26,19 @@ export default {
 body {
     margin: 0;
     background-color: #f7f4ef;
+}
+
+.left {
+    width: 45%;
+    height: 100vh;
+    float: left;
+    padding-left: 20px;
+}
+
+.right {
+    width: 45%;
+    height: 100vh;
+    float: right;
+    padding-right: 20px;
 }
 </style>
